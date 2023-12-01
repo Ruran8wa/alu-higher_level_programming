3b0ce95f6bcadba0d75e1ec786fdf04c7d15b347
ruran8wa <p.rurangwa@alustudent.com>
2023-12-01 09:21:17 -0800

    s

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response
-curl -s "$1" | wc -c
+#!/bin/bash 
+# take in a url and display the content 
+curl -sI "$1" | grep 'Content-Length'| cut -d " " -f2
