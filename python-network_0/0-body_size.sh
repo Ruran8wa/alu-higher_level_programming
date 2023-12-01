e489b4860e2638bd850b50c6c7077942bc8a7f25
ruran8wa <p.rurangwa@alustudent.com>
2023-12-01 09:39:31 -0800

    D

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash 
-# take in a url and display the content 
-curl -sI "$1" | grep 'Content-Length'| cut -d " " -f2
+#!/bin/bash
+# This script takes in a URL, sends a request using curl, and displays the content of the body of the response.
+curl -Ls "$1"
