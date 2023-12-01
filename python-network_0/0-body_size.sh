571e10cd968daba6b22e5ceee4a06dd9934892dd
ruran8wa <p.rurangwa@alustudent.com>
2023-12-01 10:09:05 -0800

    dsd

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request using curl, and displays the content of the body of the response.
-<<<<<<< HEAD
-<<<<<<< HEAD
-curl -Ls "$1"
-=======
-curl -s "$1"
->>>>>>> f8c81557828455e12f6adc4112a56adcb0f11533
-=======
-curl -Ls "$1"
->>>>>>> 35b7285493ade30fa00ba39999a5e78396924b94
+# getting content and displaying just content lenght information 
+curl -sI "$1" | grep "Content-Length" | cut -d' ' -f 2
