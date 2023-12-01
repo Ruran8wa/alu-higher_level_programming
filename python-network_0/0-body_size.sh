c69022d990f118162bd756b226d1691201baa126
Prince Rurangwa <p.rurangwa@alustudent.com>
2023-12-01 19:35:47 +0200

    d

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash 
-# take in a url and display the content 
-curl -sI "$1" | grep 'Content-Length'| cut -d " " -f2
+#!/bin/bash
+# This script takes in a URL, sends a request using curl, and displays the size of the body of the response in bytes.
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
